e0f57a1e973cac9537fc6b74903b957b851b31cf
Alex Speleersaaasdfass <alexsieger@gmail.com>
2019-01-29 20:32:49 +0200

    (ShoppingCartActions)type used in a using statement must be implicity convertible to system.Idisposable пробував не обгортати в using ();

---
+++ ComputerStore/AddToCart.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using ComputerStore.Logic;
+
+namespace ComputerStore
+{
+	public partial class AddToCart : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			string rawId = Request.QueryString["ProductID"]; //дістаємо айді продукту і присвоюємо у змінну
+			int productId;
+			if (!String.IsNullOrEmpty(rawId) && int.TryParse(rawId, out productId))
+			{
+				using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+				{
+					//визиває метод AddToCart коли створено екземпляр корзини
+					//після добавлення продукту у корзину, ця стрінка перенаправляє до ShoppingCart.aspx,
+					//з оновленою інфою
+					usersShoppingCart.AddToCart(Convert.ToInt16(rawId));
+				}
+			}
+
+			else
+			{
+				Debug.Fail("error не можна переходити на сторінку AddToCart без ProductId");
+				throw new Exception("error");
+			}
+			Response.Redirect("ShoppingCart.aspx");
+		}
+	}
+}
---
+++ ComputerStore/Models/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputerStore.Models
+{
+	public class CartItem
+	{
+		[Key]
+		public string ItemId { get; set; }//main key
+
+		public string CartId { get; set; }// match to user/customer ID
+
+		public int Quantity { get; set; }
+
+		public System.DateTime DateCreated { get; set; }
+
+		public int ProductId { get; set; }
+
+		public virtual Product Product { get; set; }
+	}
+}
